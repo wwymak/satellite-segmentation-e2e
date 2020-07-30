3acb3a3056d3e810974185d951b4f3d1753231f2
wwymak <wwymak@gmail.com>
2020-07-31 00:42:46 +0100

    some more testing/figuring out how catalyst works

--- modelling/dataflow/dataloaders.py
+++ modelling/dataflow/dataloaders.py
@@ -34,6 +34,7 @@ def get_train_val_loaders(
         summary_data_df = summary_data_df[summary_data_df.PolygonWKT_Geo != "POLYGON EMPTY"]
     print(summary_data_df.shape)
     image_ids = summary_data_df.ImageId.unique()
+    np.random.seed(11)
     np.random.shuffle(image_ids)
 
     train_image_ids = image_ids[: int(train_ratio * len(image_ids))]
@@ -79,29 +80,34 @@ def get_train_val_loaders(
 
     return train_loader, val_loader, train_eval_loader
 
-#
-# def get_inference_dataloader(
-#     root_path: str,
-#     mode: str,
-#     transforms: Callable,
-#     batch_size: int = 16,
-#     num_workers: int = 8,
-#     pin_memory: bool = True,
-#     limit_num_samples: Optional[int] = None,
-# ) -> DataLoader:
-#     assert mode in ("train", "test"), "Mode should be 'train' or 'test'"
-#
-#     get_dataset_fn = get_train_dataset if mode == "train" else get_val_dataset
-#
-#     dataset = get_dataset_fn(root_path, return_meta=True)
-#
-#     if limit_num_samples is not None:
-#         indices = np.random.permutation(len(dataset))[:limit_num_samples]
-#         dataset = Subset(dataset, indices)
-#
-#     dataset = TransformedDataset(dataset, transform_fn=transforms)
-#
-#     loader = DataLoader(
-#         dataset, shuffle=False, batch_size=batch_size, num_workers=num_workers, pin_memory=pin_memory, drop_last=False
-#     )
-#     return loader
+
+def get_inference_dataloader(
+    image_dir: Path,
+    mask_dir: Optional[Path] = None,
+    summary_data_filepath: Optional[Path] = None,
+    transforms: Optional[Callable] = None,
+    preprocessing: Optional[Callable] = None,
+    batch_size: int = 16,
+    num_workers: int = 8,
+    drop_empty_images: Optional[bool] = True,
+    pin_memory: bool = True,
+    limit_num_samples: Optional[int] = None,
+) -> DataLoader:
+
+    summary_data_df = pd.read_csv(summary_data_filepath)
+    if drop_empty_images:
+        summary_data_df = summary_data_df[summary_data_df.PolygonWKT_Geo != "POLYGON EMPTY"]
+    image_ids = summary_data_df.ImageId.unique()
+
+    ds = SatelliteSegmentationDataset(
+        image_dir, mask_dir, image_id_list=image_ids,
+        transform=transforms, preprocessing=preprocessing)
+
+    if limit_num_samples is not None:
+        indices = np.random.permutation(len(ds))[:limit_num_samples]
+        dataset = Subset(ds, indices)
+
+    loader = DataLoader(
+        dataset, shuffle=False, batch_size=batch_size, num_workers=num_workers, pin_memory=pin_memory, drop_last=False
+    )
+    return loader
--- modelling/training/test_training.py
+++ modelling/training/test_training.py
@@ -1,36 +1,25 @@
 from typing import Callable, List, Tuple
-
+import collections
 import os
 import torch
 import catalyst
 
 from catalyst.dl import utils
 from torch import nn
+from torch import optim
 
 from catalyst.contrib.nn import DiceLoss, IoULoss
+from catalyst.dl import SupervisedRunner
 
-# we have multiple criterions
-criterion = {
-    "dice": DiceLoss(),
-    "iou": IoULoss(),
-    "bce": nn.BCEWithLogitsLoss()
-}
+import segmentation_models_pytorch as smp
 
-from torch import optim
+from catalyst.utils import load_checkpoint, unpack_checkpoint
 
 from catalyst.contrib.nn import RAdam, Lookahead
 
-learning_rate = 0.001
-encoder_learning_rate = 0.0005
 
-# Since we use a pre-trained encoder, we will reduce the learning rate on it.
-layerwise_params = {"encoder*": dict(lr=encoder_learning_rate, weight_decay=0.00003)}
-
-# This function removes weight_decay for biases and applies our layerwise_params
-model_params = utils.process_model_params(model, layerwise_params=layerwise_params)
-
-# Catalyst has new SOTA optimizers out of box
-base_optimizer = RAdam(model_params, lr=learning_rate, weight_decay=0.0003)
-optimizer = Lookahead(base_optimizer)
-
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.25, patience=2)
+from pathlib import Path
+from models.unets import unet_resnet
+from dataflow.dataloaders import get_train_val_loaders
+from dataflow.visualisations import tensor_to_rgb
+from dataflow.transforms import get_train_augmentation, get_validation_augmentation, prepare_batch_fp32, get_preprocessing
